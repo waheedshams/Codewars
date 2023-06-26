bcf682a717c47af00a35a6b42af80d0bb2524191
waheed Khan <63735916+waheedshams@users.noreply.github.com>
2023-06-26 14:41:27 +0430

    Add files via upload
    solved super duper easy 8kyu

--- 8-kyu/super duper easy.js
+++ 8-kyu/super duper easy.js
@@ -2,8 +2,6 @@
 //  If the value entered is a string it should return "Error".
 
 function problem(x) {
-    //your code here
-    console.log(x)
   if(typeof x != "number"){
       return "Error"}
   else{
